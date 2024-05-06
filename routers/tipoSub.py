4638f01bf3081d0109e86ba87a1c6f6f09bb0ca2
Pc Alex <dam112.2022@gmail.com>
2024-05-06 12:50:16 +0200

    searchTipoSub

--- routers/tipoSub.py
+++ routers/tipoSub.py
@@ -1,7 +1,17 @@
 from fastapi import APIRouter
+from db.schemas.tipoSub import tipoSubSchema , tipoSubsSchema
+from db.models.tipoSub import tipoSub
+from db.client import db
 
 router = APIRouter(prefix="/tipoSub", tags=["tipoSub"],responses={404: {"message" : "No encontrado"}})
 
 @router.get("")
 async def tiposub():
     return { "message" : "Hola tipos de sub" }
+
+def searchTipoSub(field :str, key):
+    try:
+        tipousub = db.tipoSub.find_one({field: key})
+        return tipoSub(**tipoSubSchema(tipousub))
+    except:
+        return { "error" : "No se ha encontrado el tipo de subscripcion"}
